841aef372b58c4f65baedab9f4a9a65245b3c345
manishankar-0208 <manishankarreddy.ramapuram@my.jcu.edu.au>
2025-10-08 16:37:28 +1000

    Added new logic to list_exercises.py on prac 4

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,3 +12,14 @@ print(f"The last Number is {numbers[-1]}")
 print(f"The smallest of the numbers is {min(numbers)}")
 print(f"The largest of the numbers is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+
+# 2. Woefully inadequate security checker
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
