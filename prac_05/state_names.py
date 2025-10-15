17cde919f45c0d6fed5db9447dfdf685269e5839
manishankar-0208 <manishankarreddy.ramapuram@my.jcu.edu.au>
2025-10-15 15:41:47 +1000

    Update state_names.py logic on prac 5

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,28 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+# Get user input as state code and display state name
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
+# Prints all the states Codes and Names neatly lined up
+max_code_len = max(len(code) for code in CODE_TO_NAME)
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:{max_code_len}} is", name)
