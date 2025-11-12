174fbd4ea286ab9dcaac8f139ed2747083c939bd
manishankar-0208 <manishankarreddy.ramapuram@my.jcu.edu.au>
2025-11-12 22:41:45 +1000

    Implement greet and clear button functionality in BoxLayout demo and Kv file

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,9 +3,22 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Kivy app using BoxLayout with buttons, text input, and a label."""
     def build(self):
+        """Load and return the KV layout."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def handle_greet(self):
+        """Display a greeting message with the entered name."""
+        print("greet")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """Clear the text input and output label."""
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
 
 BoxLayoutDemo().run()
