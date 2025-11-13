777e7e62fd518f88da8f10c740e02fc67c95a0f9
manishankar-0208 <manishankarreddy.ramapuram@my.jcu.edu.au>
2025-11-13 10:58:24 +1000

    Update the optimised logic of convert_miles_km.py to prac08

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,41 +8,36 @@ Started 13/11/2025
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.properties import StringProperty
 
 FACTOR_MILES_TO_KM = 1.60934
 
 class MilesToKmConverterApp(App):
     """Miles to Kilometres Converter"""
+    output_km = StringProperty('Result appears here')
 
     def build(self):
         Window.size = (400, 300)
         self.title = "Convert Miles to Kilometres"
-        self.output_km = "Result appears here"
         return Builder.load_file('convert_miles_km.kv')
 
     def convert_miles_to_km(self, text):
         """Convert miles to kilometres and update label"""
         try:
             miles = float(text)
-            km = miles * FACTOR_MILES_TO_KM
-            self.output_km = f"{km:.2f}"
-            self.root.ids.output_label.text = self.output_km
         except ValueError:
-            self.output_km = "Invalid input!"
-            self.root.ids.output_label.text = self.output_km
+            miles = 0
+        km = miles * FACTOR_MILES_TO_KM
+        self.output_km = f"{km:.2f}"
 
     def handle_increment(self, change):
         """Increment or decrement input"""
         try:
-            value = float(self.root.ids.input_number.text) + change
-            self.root.ids.input_number.text = str(value)
-            self.update_result(str(value))
+            result = float(self.root.ids.input_number.text)
         except ValueError:
-            self.root.ids.input_number.text = '0'
-
-    def update_result(self, text):
-        """Update km result automatically"""
-        print("update")
-        self.convert_miles_to_km(text)
+            result = 0
+        result += change
+        self.root.ids.input_number.text = str(result)
+        self.convert_miles_to_km(str(result))
 
 MilesToKmConverterApp().run()
