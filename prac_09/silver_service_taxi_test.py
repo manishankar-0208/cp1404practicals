1d8b560f75b7980e971bd818fe4e2b6c27432ea0
manishankar-0208 <manishankarreddy.ramapuram@my.jcu.edu.au>
2025-11-19 19:41:34 +1000

    Add silver_service_taxi.py and silver_service_taxi_test.py to prac_09

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+SilverServiceTaxi class tests
+"""
+
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test SilverServiceTaxi."""
+    taxi = SilverServiceTaxi("Hummer", 200, 4)
+    taxi.drive(0)
+    print(taxi)
+    print(taxi.get_fare())
+    taxi = SilverServiceTaxi("Silver Service Fancy Taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
